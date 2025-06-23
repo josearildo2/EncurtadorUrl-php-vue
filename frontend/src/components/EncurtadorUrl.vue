b18e0c591811d798a779a7449772cd22e75c0c99
Jose Arildo <josearildo.fabres@hotmail.com>
2025-06-23 10:41:18 -0300

    Adicionando comentarios no template

--- frontend/src/components/EncurtadorUrl.vue
+++ frontend/src/components/EncurtadorUrl.vue
@@ -1,23 +1,30 @@
 <template>
+
   <div class="flex flex-col justify-center items-center min-h-screen">
+
     <div class="p-1 flex flex-row gap-4">
       <img src="/vite.svg" class="h-10" alt="Vite logo" />
       <img src="../assets/vue.svg" class="h-10" alt="Vue logo" />
     </div>
+    
     <div class="p-8 flex flex-col items-center">
       <h1 class="text-2xl font-bold mb-4">Encurtador de URL com VueJS</h1>
       <input v-model="url" placeholder="Digite a URL" class="border p-2 w-full mb-4 rounded-sm" />
       <span v-if="error" class="text-lg dark:text-red-300 mb-4">{{ error }}</span>
       
       <div>
-        <button @click="encurtar" type="button" class="bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-blue-700">Encurtar</button>
+        <button @click="encurtar" type="button" 
+          class="bg-blue-500 text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-blue-700">
+          Encurtar
+        </button>
         <button v-if="url || resultado" @click="limpar" type="button" 
           class="bg-yellow-500 text-white px-4 py-2 rounded hover:cursor-pointer hover:bg-yellow-700 ml-3"> 
           Limpar
         </button>
       </div>
 
-      <div v-if="loading" class="mt-4 w-full border-2 border-white-300 p-3 rounded-lg">
+      <!-- Div que exibe uma animação de carregamento ao fazer a requisição para o backend -->
+      <div v-if="loading" class="mt-4 w-full border-1 border-white-300 p-3 rounded-sm">
           <div class="animate-pulse">
             <div class="space-y-3">
               <div class="h-1 rounded bg-gray-100"></div>
@@ -25,12 +32,17 @@
             </div>
           </div>
       </div>
+
+      <!-- A div de carregamento é ocultada logo após o backend enviar a resposta ao frontend -->
       <div v-if="resultado && !loading" class="mt-4 w-full border-2 border-white-300 p-3 rounded-lg">
         <p><strong>Original:</strong> {{ resultado.original }}</p>
         <p><strong>Encurtada:</strong> <a :href="resultado.short" target="_blank">{{ resultado.short }}</a></p>
       </div>
+
     </div>
+
   </div>
+
 </template>
 
 <script setup>
